fd2af213876b325d995f753b2dd836281968a226
oraulinepuig <oceane.raulinepuig@gmail.com>
2023-01-17 18:47:19 +0100

    exo11 modified and fixed

--- exo_11.rb
+++ exo_11.rb
@@ -1,16 +1,21 @@
 require 'date'
 
-puts "Quelle est votre âge ?"
+puts "Quel est votre âge ?"
 user_age = gets.chomp.to_i
 current_year = DateTime.now.year
 user_birth_year = current_year - user_age 
 age = 0
-while (user_birth_year <= current_year)
-  print "Il y a "
-  print current_year - user_birth_year   
-  print " ans, tu avais "
-  print age
-  print " ans"
-  user_birth_year += 1
-  age += 1
+
+user_age.times do
+  if age == 0
+    puts "Il y a #{user_age} ans, tu venais de naître."
+    elsif age == 1
+        puts "Il y a #{user_age} ans, tu avais #{age} an."
+    elsif user_age == 1
+        puts "Il y a #{user_age} an, tu avais #{age} ans."         
+    else
+        puts "Il y a #{user_age} ans, tu avais #{age} ans."
+    end
+    user_age -=1
+    age += 1
 end
